8c7aa4ab9c05ea826fa6e400d10bcfb2cc1612b0
nischith06-coder <nischithrprakashrst221106@gmail.com>
2026-06-07 10:23:27 +0530

    Implemented rectangle and line drawing functions

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define WIDTH 50
 #define HEIGHT 20
@@ -22,8 +23,65 @@ void displayCanvas() {
     }
 }
 
+void plot(int x, int y) {
+    if(x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT) {
+        canvas[y][x] = '*';
+    }
+}
+
+void drawRectangle(int x1, int y1, int x2, int y2) {
+
+    for(int x = x1; x <= x2; x++) {
+        plot(x, y1);
+        plot(x, y2);
+    }
+
+    for(int y = y1; y <= y2; y++) {
+        plot(x1, y);
+        plot(x2, y);
+    }
+}
+
+void drawLine(int x1, int y1, int x2, int y2) {
+
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+
+    int sx = (x1 < x2) ? 1 : -1;
+    int sy = (y1 < y2) ? 1 : -1;
+
+    int err = dx - dy;
+
+    while(1) {
+
+        plot(x1, y1);
+
+        if(x1 == x2 && y1 == y2)
+            break;
+
+        int e2 = 2 * err;
+
+        if(e2 > -dy) {
+            err -= dy;
+            x1 += sx;
+        }
+
+        if(e2 < dx) {
+            err += dx;
+            y1 += sy;
+        }
+    }
+}
+
 int main() {
+
     clearCanvas();
+
+    drawRectangle(5, 3, 20, 10);
+
+    drawLine(0, 0, 40, 15);
+
     displayCanvas();
+
     return 0;
 }
