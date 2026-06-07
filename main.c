7c2346a0ff818ffaccfc68a7c52e2822328c51eb
nischith06-coder <nischithrprakashrst221106@gmail.com>
2026-06-07 10:20:17 +0530

    Added canvas initialization and display functions

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
- main() {
-    printf("2D Graphics Editor\n");
+#define WIDTH 50
+#define HEIGHT 20
+
+char canvas[HEIGHT][WIDTH];
+
+void clearCanvas() {
+    for(int i = 0; i < HEIGHT; i++) {
+        for(int j = 0; j < WIDTH; j++) {
+            canvas[i][j] = '_';
+        }
+    }
+}
+
+void displayCanvas() {
+    for(int i = 0; i < HEIGHT; i++) {
+        for(int j = 0; j < WIDTH; j++) {
+            printf("%c", canvas[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    clearCanvas();
+    displayCanvas();
     return 0;
 }
